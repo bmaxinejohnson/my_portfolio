b78b7f4c041d61c1e7b1c9410e7a56fc5233547c
bmaxinejohnson <bmaxinejohnson@outlook.com>
2024-08-10 15:48:40 -0400

    Updated personal info

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,15 +1,15 @@
 import { NavLink } from 'react-router-dom';
-import PlaceholderImage from '../assets/placeholder_image.png';
+import PlaceholderImage from '../src/assets/headshot2.jpg';
 
 // Replace these variables with your information
-const firstName = 'Replace with your first name';
-const jobTitle = 'Replace with your title';
-const company = 'Replace with your company name';
-const mainDuty = 'Replace with your main duty';
-const jobLocation = 'Replace with your job location';
-const briefJobDescription = 'Replace with a brief description of your job';
-const careerObjective = 'Replace with your career objective';
-const personalLife = 'Replace with a brief description of your personal life';
+const firstName = 'Brianna';
+const jobTitle = 'Assistant Manager';
+const company = 'Novadoz Pharmaceuticals';
+const mainDuty = 'Account Services and Sales Operations';
+const jobLocation = 'Piscataway, NJ.';
+const briefJobDescription = 'Armed with a Bachelors degree in Health Care Administration from Rutgers University, I manage top accounts, lead special projects, and foster strong relationships with National and Territory Account Managers by creating efficient workflows and providing guidance to customer service associates. I present monthly analysis to upper management, suggesting strategies for improved performance and system enhancements. Client Relations and Support is my jam, and I love diving into CRM systems to ensure top-notch satisfaction.';
+const careerObjective = 'I am currently looking to work for an amazing company doing meaningful work. Preferably in the tech industry or tech adjacent. I am also open to contract and consulting work';
+const personalLife = 'Outside of work I am a proud dog mommy to Reesie, you will find me volunteering, mingling in professional circles, always hungry for new connections and opportunities for growth. Lets connect and chat about how we can make an impact together! 🚀';
 
 const About = () => {
   return (
@@ -20,7 +20,7 @@ const About = () => {
           <img className='headshot' src={PlaceholderImage} alt="placeholder image" />
         </section>
         <section className='description-container'>
-          <h2 className='text-xl font-medium'>I&apos;m {firstName}, a {jobTitle} working for, {company} on {mainDuty} located in, {jobLocation}</h2>
+          <h2 className='text-xl font-medium'>I&apos;m {firstName}, an {jobTitle} working for {company} on {mainDuty} located in {jobLocation}</h2>
           <p>{briefJobDescription}</p>
           <p>{careerObjective}</p>
           <p>{personalLife}</p>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,10 @@
 import { NavLink } from "react-router-dom";
-import PlaceholderHero from '../assets/placeholder_hero_background.jpg'
+import PlaceholderHero from '../assets/yellow.jpg'
+import PlaceholderHero2 from '../assets/crumbled_paper.jpg'
 
-const fullName = 'Replace with your name';
-const personalQuote = 'Replace with your personal quote';
-const professionalSummary = 'Replace with your professional summary';
+const fullName = 'Brianna Johnson';
+const personalQuote = "Eat . Sleep . Code . Repeat.";
+const professionalSummary = 'Customer-focused IT Professional with over five years of experience in driving customer satisfaction and retention';
 
 const Home = () => {
   return (
@@ -15,12 +16,13 @@ const Home = () => {
         Contact Me
         </NavLink>
         {/* Replace the src with your own placeholder and change the alt to match the description */}
-        <img className="hero-background" src={PlaceholderHero} alt="placeholder hero image" />
+        <img className="hero-background" src={PlaceholderHero} alt="chalkboard image" />
       </section>
       <section className="overview home-subsection">
         <h3 className="subsection-intro-header">Introduction</h3>
         <h2 className="subsection-big-header">Overview.</h2>
         <p className="subsection-paragraph">{professionalSummary}</p>
+        
         <NavLink to="/about">
         Learn More About Me
         </NavLink>
@@ -28,7 +30,7 @@ const Home = () => {
       <section className="projects home-subsection">
         <h3 className="subsection-intro-header">My Works</h3>
         <h2 className="subsection-big-header">Projects.</h2>
-        <p className="subsection-paragraph">I have worked on multiple project, the projects I have worked on demonstrate my skills and experience through real-life examples of my work. Each project is briefly deescribed and includes links to code repositories that showcase my ability to tackle challenging problems. If you are interested in my work click on the link below to go to the project page.</p>
+        <p className="subsection-paragraph">I have worked on multiple project, the projects I have worked on demonstrate my skills and experience through real-life examples of my work. Each project is briefly described and includes links to code repositories that showcase my ability to tackle challenging problems. If you are interested in my work click on the link below to go to the project page.</p>
         <NavLink to="/projects">
         View Projects
         </NavLink>
